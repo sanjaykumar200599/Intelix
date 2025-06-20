b3365005301e463ee15ff6639445745503c16a70
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-06-20 12:34:54 +0530

    handling response

--- main.py
+++ main.py
@@ -32,6 +32,14 @@ async def handle_message(update: Update,context:ContextTypes.DEFAULT_TYPE ):
     message_type: str = upadte.message.chat.type
     text: str = update.message.text
     print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
-    
 
+    if message_type =='group':
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, '').strip()
+            response: str = handle_response(new_text)
+        else:
+            return
+    else:
+            response: str =handle_response(text)
+            
 
